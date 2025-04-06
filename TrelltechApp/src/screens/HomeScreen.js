54f462b057cf06cf241055130f304336c848e122
Kan-à-Pé <alexandre.mandon0@gmail.com>
2025-04-06 23:41:58 +0200

    style page home

--- TrelltechApp/src/screens/HomeScreen.js
+++ TrelltechApp/src/screens/HomeScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bienvenue sur l'écran d'accueil </Text>
-      <Button title="Voir mes Workspaces" onPress={() => navigation.navigate("Workspaces")} />
+      <Text style={styles.text}>Bienvenue sur l'écran d'accueil</Text>
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={() => navigation.navigate("Workspaces")}
+      >
+        <Text style={styles.buttonText}>Voir mes Workspaces</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -15,13 +20,32 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f8f9fa",
+    backgroundColor: "#e0f7fa", 
+    padding: 20,
   },
   text: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#333",
-    marginBottom: 20,
+    fontSize: 24,
+    fontWeight: "600",
+    color: "#2c3e50", 
+    marginBottom: 40,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#3498db", // Bleu
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 6, 
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#fff",
+    textAlign: "center",
   },
 });
 
